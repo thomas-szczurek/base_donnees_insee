4393ba27742cd841ea525bc4bcb29c23f8d8ec76
thomas-szczurek <121474664+thomas-szczurek@users.noreply.github.com>
2024-05-03 11:20:11 +0200

    Update zonages_administratifs.sql

--- supabase/sql/creation_tables/zonages_administratifs.sql
+++ supabase/sql/creation_tables/zonages_administratifs.sql
@@ -7,5 +7,5 @@ CREATE TABLE insee.zonages_administratifs (
 	annee_fin_validite int2 NOT NULL,
 	geom geometry(POLYGON, 2154),
 	CONSTRAINT fk_type_zonage FOREIGN KEY (fk_type) REFERENCES insee.types_zonage (pk_id) MATCH SIMPLE ON UPDATE CASCADE ON DELETE CASCADE,
-	CONSTRAINT fk_zonage_code FOREIGN KEY (code_commune) REFERENCES insee.codes_admin (code_admin) ON UPDATE NO ACTION ON DELETE NO ACTION
+	CONSTRAINT fk_zonage_code FOREIGN KEY (code_admin) REFERENCES insee.codes_admin (code_admin) ON UPDATE NO ACTION ON DELETE NO ACTION
   ) tablespace pg_default;
