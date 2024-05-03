3bb401fd568d482223a6c26ac642439d256a92bd
thomas-szczurek <121474664+thomas-szczurek@users.noreply.github.com>
2024-05-03 13:30:01 +0200

    Update appartenance_zonages.sql

--- supabase/sql/creation_tables/appartenance_zonages.sql
+++ supabase/sql/creation_tables/appartenance_zonages.sql
@@ -1,7 +1,7 @@
 CREATE TABLE insee.appartenance_zonages (
 	pk_id int4 GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-	code_admin_parent int4 NOT NULL,
-	code_admin_enfant int4 NOT NULL,
+	code_admin_parent text NOT NULL,
+	code_admin_enfant text NOT NULL,
 	annee_debut_validite int2 NOT NULL,
 	annee_fin_validite int2,
 	CONSTRAINT fk_zonage_parent FOREIGN KEY (code_admin_parent) REFERENCES insee.codes_admin (code_admin) ON DELETE NO ACTION ON UPDATE CASCADE,
