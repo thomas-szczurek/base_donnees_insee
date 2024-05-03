db79eb1d89809fc0d2265223711ce5646dbbea8f
thomas-szczurek <121474664+thomas-szczurek@users.noreply.github.com>
2024-05-03 13:29:07 +0200

    Update appartenance_zonages.sql

--- supabase/sql/creation_tables/appartenance_zonages.sql
+++ supabase/sql/creation_tables/appartenance_zonages.sql
@@ -1,11 +1,9 @@
 CREATE TABLE insee.appartenance_zonages (
 	pk_id int4 GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-	fk_zonage_parent int4 NOT NULL,
-	fk_zonage_enfant int4 NOT NULL,
-	fk_type_zonage int2 NOT NULL,
+	code_admin_parent int4 NOT NULL,
+	code_admin_enfant int4 NOT NULL,
 	annee_debut_validite int2 NOT NULL,
 	annee_fin_validite int2,
-	CONSTRAINT fk_zonage_parent FOREIGN KEY (fk_zonage_parent) REFERENCES insee.zonages_administratifs (pk_id) ON DELETE NO ACTION ON UPDATE CASCADE,
-	CONSTRAINT fk_zonage_enfant FOREIGN KEY (fk_zonage_enfant) REFERENCES insee.zonages_administratifs (pk_id) ON DELETE NO ACTION ON UPDATE CASCADE,
-	CONSTRAINT fk_type_zonage FOREIGN KEY (fk_type_zonage)  REFERENCES insee.types_zonage (pk_id) ON DELETE CASCADE ON UPDATE CASCADE
+	CONSTRAINT fk_zonage_parent FOREIGN KEY (code_admin_parent) REFERENCES insee.codes_admin (code_admin) ON DELETE NO ACTION ON UPDATE CASCADE,
+	CONSTRAINT fk_zonage_enfant FOREIGN KEY (code_admin_enfant) REFERENCES insee.codes_admin (code_admin) ON DELETE NO ACTION ON UPDATE CASCADE
 );
