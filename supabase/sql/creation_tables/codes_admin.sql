7ed18061046701bb7c11346b8d197ed0ff098ec1
thomas-szczurek <121474664+thomas-szczurek@users.noreply.github.com>
2024-05-03 11:17:30 +0200

    Update codes_admin.sql

--- supabase/sql/creation_tables/codes_admin.sql
+++ supabase/sql/creation_tables/codes_admin.sql
@@ -1,5 +1,4 @@
 CREATE TABLE insee.codes_admin (
 	pk_id int4 PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-	code_commune text UNIQUE NOT NULL,
-	CONSTRAINT fk_zonage_code FOREIGN KEY (code_commune) REFERENCES insee.zonages_administratifs (code_admin) ON UPDATE NO ACTION ON DELETE NO ACTION
+	code_admin text UNIQUE NOT NULL
 );
