dd688a38fc4ddb347f455e120219fa1f9e99e32d
thomas-szczurek <121474664+thomas-szczurek@users.noreply.github.com>
2024-07-07 11:54:21 +0200

    Update donnees_communes.sql

--- supabase/sql/creation_tables/donnees_communes.sql
+++ supabase/sql/creation_tables/donnees_communes.sql
@@ -7,13 +7,14 @@ CREATE TABLE insee.donnees_communes (
 	donnees jsonb NOT NULL,
 	CONSTRAINT fk_donnees_communes FOREIGN KEY (code_commune) REFERENCES insee.codes_admin (code_admin) ON UPDATE CASCADE ON DELETE NO ACTION,
 	CONSTRAINT fk_donnees_bases FOREIGN KEY (fk_base) REFERENCES insee.bases (pk_id) ON UPDATE CASCADE ON DELETE CASCADE
-	CONSTRAINT unique_annee_fk_base_com UNIQUE (annee, fk_base, code_commune)
 ) PARTITION BY LIST (fk_base);
 CREATE TABLE insee.donnees_communes_fk_1 PARTITION OF insee.donnees_communes FOR VALUES IN (1);
-CREATE TABLE insee.donnees_communes_fk_1 PARTITION OF insee.donnees_communes FOR VALUES IN (2);
-CREATE TABLE insee.donnees_communes_fk_1 PARTITION OF insee.donnees_communes FOR VALUES IN (3);
-CREATE TABLE insee.donnees_communes_fk_1 PARTITION OF insee.donnees_communes FOR VALUES IN (4);
-CREATE TABLE insee.donnees_communes_fk_1 PARTITION OF insee.donnees_communes FOR VALUES IN (5);
-CREATE TABLE insee.donnees_communes_fk_1 PARTITION OF insee.donnees_communes FOR VALUES IN (6);
-CREATE TABLE insee.donnees_communes_fk_1 PARTITION OF insee.donnees_communes FOR VALUES IN (7);
+CREATE TABLE insee.donnees_communes_fk_2 PARTITION OF insee.donnees_communes FOR VALUES IN (2);
+CREATE TABLE insee.donnees_communes_fk_3 PARTITION OF insee.donnees_communes FOR VALUES IN (3);
+CREATE TABLE insee.donnees_communes_fk_4 PARTITION OF insee.donnees_communes FOR VALUES IN (4);
+CREATE TABLE insee.donnees_communes_fk_5 PARTITION OF insee.donnees_communes FOR VALUES IN (5);
+CREATE TABLE insee.donnees_communes_fk_6 PARTITION OF insee.donnees_communes FOR VALUES IN (6);
+CREATE TABLE insee.donnees_communes_fk_7 PARTITION OF insee.donnees_communes FOR VALUES IN (7);
+CREATE TABLE insee.donnees_communes_fk_8 PARTITION OF insee.donnees_communes FOR VALUES IN (8);
+CREATE TABLE insee.donnees_communes_fk_9 PARTITION OF insee.donnees_communes FOR VALUES IN (9);
 END;
