1cccb9a30c3e3a9b55fd22f403988199fe8b0abf
Thomas Szczurek <thomas.szczurek@gmail.com>
2024-07-09 10:52:16 +0200

    update vue annees presence clefs

--- supabase/sql/vues/vue_presence_clefs_annees.sql
+++ supabase/sql/vues/vue_presence_clefs_annees.sql
@@ -1,102 +1,13 @@
 -- Vue supabase permettant de récupérer les années minimum et maximum de présence de chaque clef
 CREATE VIEW insee.presence_clefs_annees AS
 -- On utilise les UNION pour profiter du partionnement de donnees_communes et accelérer la requête
+CREATE VIEW insee.presence_clefs_annees AS
 SELECT
-	clef_json,
-  1 AS fk_base,
-	(SELECT min(annee) FROM insee.donnees_communes WHERE fk_base = 1 AND donnees ? clef_json) AS premiere_annee_presence,
-	(SELECT max(annee) FROM insee.donnees_communes WHERE fk_base = 1 AND donnees ? clef_json) AS derniere_annee_presence
-FROM
-	insee.correspondance_clefs_champs
-WHERE fk_base = 1
-UNION
-SELECT
-	clef_json,
-  2 AS fk_base,
-	(SELECT min(annee) FROM insee.donnees_communes WHERE fk_base = 2 AND donnees ? clef_json) AS premiere_annee_presence,
-	(SELECT max(annee) FROM insee.donnees_communes WHERE fk_base = 2 AND donnees ? clef_json) AS derniere_annee_presence
-FROM
-	insee.correspondance_clefs_champs
-WHERE fk_base = 2
-UNION
-SELECT
-	clef_json,
-  2 AS fk_base,
-	(SELECT min(annee) FROM insee.donnees_communes WHERE fk_base = 3 AND donnees ? clef_json) AS premiere_annee_presence,
-	(SELECT max(annee) FROM insee.donnees_communes WHERE fk_base = 3 AND donnees ? clef_json) AS derniere_annee_presence
-FROM
-	insee.correspondance_clefs_champs
-WHERE fk_base = 3
-UNION
-SELECT
-	clef_json,
-  3 AS fk_base,
-	(SELECT min(annee) FROM insee.donnees_communes WHERE fk_base = 3 AND donnees ? clef_json) AS premiere_annee_presence,
-	(SELECT max(annee) FROM insee.donnees_communes WHERE fk_base = 3 AND donnees ? clef_json) AS derniere_annee_presence
-FROM
-	insee.correspondance_clefs_champs
-WHERE fk_base = 3
-UNION
-SELECT
-	clef_json,
-  4 AS fk_base,
-	(SELECT min(annee) FROM insee.donnees_communes WHERE fk_base = 4 AND donnees ? clef_json) AS premiere_annee_presence,
-	(SELECT max(annee) FROM insee.donnees_communes WHERE fk_base = 4 AND donnees ? clef_json) AS derniere_annee_presence
-FROM
-	insee.correspondance_clefs_champs
-WHERE fk_base = 4
-UNION
-SELECT
-	clef_json,
-  5 AS fk_base,
-	(SELECT min(annee) FROM insee.donnees_communes WHERE fk_base = 5 AND donnees ? clef_json) AS premiere_annee_presence,
-	(SELECT max(annee) FROM insee.donnees_communes WHERE fk_base = 5 AND donnees ? clef_json) AS derniere_annee_presence
-FROM
-	insee.correspondance_clefs_champs
-WHERE fk_base = 5
-UNION
-SELECT
-	clef_json,
-  6 AS fk_base,
-	(SELECT min(annee) FROM insee.donnees_communes WHERE fk_base = 6 AND donnees ? clef_json) AS premiere_annee_presence,
-	(SELECT max(annee) FROM insee.donnees_communes WHERE fk_base = 6 AND donnees ? clef_json) AS derniere_annee_presence
-FROM
-	insee.correspondance_clefs_champs
-WHERE fk_base = 6
-UNION
-SELECT
-	clef_json,
-  7 AS fk_base,
-	(SELECT min(annee) FROM insee.donnees_communes WHERE fk_base = 7 AND donnees ? clef_json) AS premiere_annee_presence,
-	(SELECT max(annee) FROM insee.donnees_communes WHERE fk_base = 7 AND donnees ? clef_json) AS derniere_annee_presence
-FROM
-	insee.correspondance_clefs_champs
-WHERE fk_base = 7
-UNION
-SELECT
-	clef_json,
-  8 AS fk_base,
-	(SELECT min(annee) FROM insee.donnees_communes WHERE fk_base = 8 AND donnees ? clef_json) AS premiere_annee_presence,
-	(SELECT max(annee) FROM insee.donnees_communes WHERE fk_base = 8 AND donnees ? clef_json) AS derniere_annee_presence
-FROM
-	insee.correspondance_clefs_champs
-WHERE fk_base = 8
-UNION
-SELECT
-	clef_json,
-  9 AS fk_base,
-	(SELECT min(annee) FROM insee.donnees_communes WHERE fk_base = 9 AND donnees ? clef_json) AS premiere_annee_presence,
-	(SELECT max(annee) FROM insee.donnees_communes WHERE fk_base = 9 AND donnees ? clef_json) AS derniere_annee_presence
-FROM
-	insee.correspondance_clefs_champs
-WHERE fk_base = 9
-UNION
-SELECT
-	clef_json,
-  10 AS fk_base,
-	(SELECT min(annee) FROM insee.donnees_communes WHERE fk_base = 10 AND donnees ? clef_json) AS premiere_annee_presence,
-	(SELECT max(annee) FROM insee.donnees_communes WHERE fk_base = 10 AND donnees ? clef_json) AS derniere_annee_presence
-FROM
-	insee.correspondance_clefs_champs
-WHERE fk_base = 10
+	c.clef_json AS clef_json,
+	c.fk_base AS fk_base,
+	p.premiere AS premiere_annee_presence,
+	d.derniere AS derniere_annee_presence
+FROM insee.correspondance_clefs_champs AS c,
+	LATERAL (SELECT min(annee) AS premiere FROM insee.donnees_communes WHERE fk_base = c.fk_base AND donnees ? clef_json) AS p,
+	LATERAL (SELECT max(annee) AS derniere FROM insee.donnees_communes WHERE fk_base = c.fk_base AND donnees ? clef_json) AS d
 ORDER BY fk_base, clef_json;
