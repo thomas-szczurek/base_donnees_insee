c2038ab68dffb9e2e4c4b652356e6436b88855da
thomas-szczurek <121474664+thomas-szczurek@users.noreply.github.com>
2024-10-08 16:21:14 +0200

    Update import_rp_formation.sql

--- sql/import/import_rp_formation.sql
+++ sql/import/import_rp_formation.sql
@@ -72,7 +72,7 @@ WITH d AS (
 INSERT INTO insee.donnees_communes("annee","fk_base","donnees", "code_commune")
 SELECT
 	   2020,
-	   4,
+	   5,
 	   jsonb_object(string_to_array(data::text,',')),
 	   "CODGEO"
 FROM d
