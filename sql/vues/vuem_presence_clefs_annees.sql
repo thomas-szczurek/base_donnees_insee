6aa2e9a825230466e989eb567902ae8a5fa728c1
thomas-szczurek <121474664+thomas-szczurek@users.noreply.github.com>
2024-10-08 16:29:01 +0200

    Update vuem_presence_clefs_annees.sql

--- sql/vues/vuem_presence_clefs_annees.sql
+++ sql/vues/vuem_presence_clefs_annees.sql
@@ -6,6 +6,6 @@ SELECT
 	p.premiere AS premiere_annee_presence,
 	d.derniere AS derniere_annee_presence
 FROM insee.correspondance_clefs_champs AS c,
-	LATERAL (SELECT min(annee) AS premiere FROM insee.donnees_communes WHERE fk_base = c.fk_base AND donnees ? clef_json) AS p,
-	LATERAL (SELECT max(annee) AS derniere FROM insee.donnees_communes WHERE fk_base = c.fk_base AND donnees ? clef_json) AS d
+	LATERAL (SELECT min(annee) AS premiere FROM insee.donnees_communes WHERE fk_base = c.fk_base AND donnees ? c.clef_json) AS p,
+	LATERAL (SELECT max(annee) AS derniere FROM insee.donnees_communes WHERE fk_base = c.fk_base AND donnees ? c.clef_json) AS d
 ORDER BY fk_base, clef_json;
