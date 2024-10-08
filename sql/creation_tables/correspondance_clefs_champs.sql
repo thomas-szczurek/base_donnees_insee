a142bded199648370e7d50ba126b4b73578aaa14
thomas-szczurek <121474664+thomas-szczurek@users.noreply.github.com>
2024-10-08 16:23:26 +0200

    Update correspondance_clefs_champs.sql

--- sql/creation_tables/correspondance_clefs_champs.sql
+++ sql/creation_tables/correspondance_clefs_champs.sql
@@ -4,8 +4,6 @@ CREATE TABLE insee.correspondance_clefs_champs (
 	clef_json text NOT NULL,
 	description text,
 	fk_base int2 NOT NULL,
-	premiere_annee_presence int2,
-	derniere_annee_presence int2,
     constraint correspondance_clefs_champs_nom_champ_unique unique (nom_champ_insee, fk_base),
     constraint fk_correspondance_base foreign key (fk_base) references insee.bases (pk_id) ON UPDATE NO ACTION ON DELETE NO ACTION
 );
